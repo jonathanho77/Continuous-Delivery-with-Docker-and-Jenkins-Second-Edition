7b3fe327024bd5891c2932277901a232ff7a0c55
jonathanho77 <72172329+jonathanho77@users.noreply.github.com>
2022-03-17 23:35:41 -0400

    Divide
    StepDefinitions.java Divide

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -15,22 +15,22 @@ public class StepDefinitions {
 
     private String a;
     private String b;
-    private String result;
+    private String quotient;
 
-    @Given("^I have two numbers: (.*) and (.*)$")
+    @Given("^I have dividend and divisor: (.*) and (.*)$")
     public void i_have_two_numbers(String a, String b) throws Throwable {
         this.a = a;
         this.b = b;
     }
 
-    @When("^the calculator sums them$")
-    public void the_calculator_sums_them() throws Throwable {
-        String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
-        result = restTemplate.getForObject(url, String.class);
+    @When("^the calculator divides them$")
+    public void the_calculator_divs_them() throws Throwable {
+        String url = String.format("%s/div?a=%s&b=%s", server, a, b);
+        quotient = restTemplate.getForObject(url, String.class);
     }
 
-    @Then("^I receive (.*) as a result$")
-    public void i_receive_as_a_result(String expectedResult) throws Throwable {
-        assertEquals(expectedResult, result);
+    @Then("^I receive (.*) as a quotient$")
+    public void i_receive_as_a_result(String expectedQuotient) throws Throwable {
+        assertEquals(expectedQuotient, quotient);
     }
 }
